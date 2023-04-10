3cd953c5cdcc99236aabc3593f53b7eb4022bbce
jamme05 <thenewspacerabbit@gmail.com>
2023-04-10 11:38:12 +0200

    Add project files.

---
+++ remoteControlV2Capture/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static ScreenCapture.ScreenCapture;
+
+namespace remoteControlV2Capturer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new Capturer(
+                    IPAddress.Parse(args[0]),
+                    ushort.Parse(args[1]),
+                    ushort.Parse(args[2])
+                    ).Work();
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+    }
+
+    class Capturer
+    {
+        public static double frameRate = 24;
+        public IPEndPoint endPoint1;
+        public IPEndPoint endPoint2;
+        IPAddress address;
+
+        TcpClient client;
+
+        CaptureWorker worker1;
+        CaptureWorker worker2;
+
+        Timer timer;
+        double[] fps = new double[]
+        {
+            5,
+            10,
+            20,
+            24,
+            30,
+            33
+        };
+
+
+        public Capturer(IPAddress address, ushort port1, ushort port2)
+        {
+            this.address = address;
+            endPoint1 = new IPEndPoint(address, port1);
+            endPoint2 = new IPEndPoint(address, port2);
+        }
+
+
+        public void Work()
+        {
+            timer = new Timer((e) =>
+            {
+
+            }, null, TimeSpan.Zero, TimeSpan.FromSeconds(1 / frameRate));
+        }
+
+        public void Pause()
+        {
+            timer.Dispose();
+        }
+    }
+
+    class CaptureWorker
+    {
+        public IPEndPoint endPoint;
+        public UdpClient client;
+        bool enabled = true;
+
+        public CaptureWorker(IPEndPoint endPoint)
+        {
+            this.endPoint = endPoint;
+            client = new UdpClient(endPoint);
+        }
+
+        public async void Capture(int segment = 0)
+        {
+            byte[] buffer = CaptureScreen(segment);
+
+            var mem = new MemoryStream();
+            using (var zipStream = new GZipStream(mem, CompressionMode.Compress))
+            {
+                zipStream.Write(buffer, 0, buffer.Length);
+                var compressed = mem.ToArray();
+            }
+        }
+    }
+}
---
+++ remoteControlV2Capturer/ScreenCapture.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using Encoder = System.Drawing.Imaging.Encoder;
+using ImageSharp;
+
+namespace ScreenCapture
+{
+    internal class ScreenCapture
+    {
+        private static ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+        private static EncoderParameters myEncoderParameters = new EncoderParameters(1);
+        private static Encoder myEncoder = Encoder.Quality;
+        private static EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 60L);
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+        public static byte[] CaptureScreen(int seg)
+        {
+            return CaptureWindow(User32.GetDesktopWindow(), seg);
+        }
+        public static byte[] CaptureWindow(IntPtr handle, int seg)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = (windowRect.bottom - windowRect.top) / 2;
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, height * seg, GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest, hOld);
+            // clean up 
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+            // get a .NET image object for it
+            Bitmap img = Bitmap.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+
+            using (var stream = new MemoryStream())
+            {
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                img.Save(stream, codecInfo, myEncoderParameters);
+
+                return stream.ToArray();
+            }
+        }
+
+        private static class GDI32
+        {
+
+            public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+                int nWidth, int nHeight, IntPtr hObjectSource,
+                int nXSrc, int nYSrc, int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+        }
+
+        private static class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+        }
+    }
+}
